d4e8fc63be384a5c9f0411efee1f14c3d0494dbe
balaji-githubstore <balajidinakaran1@gmail.com>
2026-01-29 10:05:53 +0530

    Addpet using playwright

--- src/test/java/com/kpmg/apitest/Demo1PostPet.java
+++ src/test/java/com/kpmg/apitest/Demo1PostPet.java
@@ -1,28 +1,37 @@
 package com.kpmg.apitest;
 
 import com.microsoft.playwright.APIRequest.NewContextOptions;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import com.microsoft.playwright.APIRequestContext;
 import com.microsoft.playwright.APIResponse;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.impl.RequestOptionsImpl;
 
 public class Demo1PostPet {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		
+		String jsonBodyStr= Files.readString(Paths.get("files/new_pet.json"));
 		
 		String baseUrl="https://petstore.swagger.io";
-		String resource="/v2/pet/10";
+		String resource="/v2/pet";
 		
 		Playwright playwright = Playwright.create();
 		
 		APIRequestContext context= playwright.request().newContext(new NewContextOptions().setBaseURL(baseUrl));
 
-		APIResponse response= context.get(resource);
+		APIResponse response= context.post(resource,
+				new RequestOptionsImpl().setHeader("Content-Type", "application/json").setData(jsonBodyStr));
 		
 		System.out.println(response.status());
 		System.out.println(response.text());
 		System.out.println(response.body());
+		
+		playwright.close();
 		
 	}
 
